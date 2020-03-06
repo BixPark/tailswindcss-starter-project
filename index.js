b75303b5ff466f1edb788b4484f0a10b4e3d9a68
Dewmal <dewmalnilanka@gmail.com>
2020-03-06 11:05:14 +0530

    Marketing Agent

--- index.js
+++ index.js
@@ -1,22 +1,9 @@
-var http = require('http'),
-    fs = require('fs'),
-    url = require('url'),
-    rootFolder = '/public/',
-    defaultFileName = '/public/index.html';
+var express = require('express');
+var app = express();
+var path = require('path');
 
-
-http.createServer(function (req, res) {
-
-    var fileName = url.parse(req.url).pathname;
-    // If no file name in Url, use default file name
-    fileName = (fileName == "/") ? defaultFileName : rootFolder + fileName;
-
-    fs.readFile(__dirname + decodeURIComponent(fileName), 'binary', function (err, content) {
-        if (content != null && content != '') {
-            res.writeHead(200, {'Content-Length': content.length});
-            res.write(content);
-        }
-        res.end();
-    });
-
-}).listen(8800);
+app.use(express.static(__dirname + '/public')); //Serves resources from public folder
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname + '/public/index.html'));
+});
+var server = app.listen(5000);
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,19 @@
+const {colors} = require('tailwindcss/defaultTheme');
+
 module.exports = {
-  theme: {
-    extend: {},
-  },
-  variants: {},
-  plugins: [],
+    theme: {
+        extend: {},
+        colors: {
+            primary: colors.indigo,
+            secondary: colors.pink,
+            primaryInverse: colors.white,
+            secondaryInverse: colors.white,
+            black: colors.black,
+            transparent: colors.transparent,
+            white: colors.white,
+            gray: colors.gray
+        }
+    },
+    variants: {},
+    plugins: [],
 }
